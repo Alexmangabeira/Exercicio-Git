d24bdc784aece8dbe620a5ca019573c2e42e0803
alexandro.turma7@gmail.com <alexandro.turma7@gmail.com>
2019-09-28 20:57:02 -0300

    Função - Números ordem crescente

---
+++ Exercicio001.js
@@ -0,0 +1,22 @@
+// Números em ordem crescente
+//https://github.com/Alexmangabeira/Exercicio.git
+function compare(a, b) {
+    if (a < b) {
+        return -1
+    }
+    if (a > b) {
+        return 1
+    }
+    return 0
+}
+
+function compareNumbers(a, b) {
+    return a - b
+}
+
+var numbers = [89,0,36,5,2,65,1,45,7]
+numbers.sort(function(a, b){
+    return a - b
+})
+
+console.log(numbers)

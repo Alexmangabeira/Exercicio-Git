2b39dfe10f443f1c23562fd0632ec95b28d4a5c6
alexandro.turma7@gmail.com <alexandro.turma7@gmail.com>
2019-10-06 20:51:50 -0300

    Analizando o maior/menor

--- Exercicio002/Exercicio002.js
+++ Exercicio002/Exercicio002.js
@@ -1,27 +1,88 @@
 // Maior e Menor
-//var vetor = new Array(5)
-var vetor = [89,5,45,1,9,55]
-var maior = 0, menor = 0
-var r = ''
+document.querySelector('input#num').focus()
+let num = document.querySelector('input#num')
+let listas = document.querySelector('select#lista')
+let resultado = document.querySelector('div#resultado')
+let valores = []
 
-for (var i = 0; i < vetor.length; i++) {
-    //vetor[i] = Number(window.prompt(`Digite ${vetor.length -i}º Números`))
 
-    if (i == 0){
-        maior = vetor[i]
-        menor = vetor[i]
+// Verificando se são números
+let isnumero = (n) => {
+    if (Number(n) > 0 && Number(n) <= 100){
+        return true
     }else{
-        if (vetor[i] > maior){
-            maior = vetor[i]
-        }
-        if (vetor[i] < menor){
-            menor = vetor[i]
-        }
+        return false
     }
-
-    r += vetor[i] + ' '
 }
 
-console.log(`Nº digitados: ${r}`)
-console.log(`Menor: ${menor}`)
-console.log(`Maior: ${maior}`)
+// Verificando se contem os números dentro da lista
+let inlista = (numeros, lista) => {
+    if (lista.indexOf(Number(numeros)) != -1) {
+        return true
+    }else{
+        return false
+    }
+}
+
+// Função adicionar na lista
+let item
+let add = () => {
+    if (isnumero(num.value) && !inlista(num.value, valores)){
+        valores.push(Number(num.value))
+        item = document.createElement('option')
+        item.text = `valor adicionado ${num.value}`
+        listas.appendChild(item)
+        resultado.innerHTML = ''
+    }else{
+        alert(`Valor invalido ou contem na lista`)
+    }
+    num.value = ''
+    num.focus()
+}
+
+// Função analizar
+let analizar = () => {
+    if (valores.length < 0){
+        alert('Digite os valores')
+    }else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma  = 0
+        let media = 0
+
+        for (let posicao in valores){
+            soma += valores[posicao]
+            if (valores[posicao] > maior) {
+                maior = valores[posicao]
+            }if (valores[posicao] < menor) {
+                menor = valores[posicao]
+            }
+        }
+        media = (soma / total)
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p>Total dos Nº digitados: ${total}</p>`
+        resultado.innerHTML += `<p>Maior: ${maior}.</p>`
+        resultado.innerHTML += `<p>Menor: ${menor}</p>`
+        resultado.innerHTML += `<p>Soma: ${soma}</p>`
+        resultado.innerHTML += `<p>Média: ${media.toFixed(2)}</p>`
+    }
+    num.value = ''
+    num.focus()
+}
+
+// Função Para limpar os campos
+let limpar = () => {
+    resultado.innerHTML = ''
+    num.focus()
+    valores = []
+    for (let pos in listas){
+        if (listas.length > 0){
+            listas.remove(valores[pos])
+        }
+    }
+}
+
+
+
+
